a3683f581566025562eb605b2570a74adc198c3d
Jeremy Block <j.block@ufl.edu>
2020-07-30 15:13:03 -0400

    Basic video component built from Video-React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import "../node_modules/video-react/dist/video-react.css"; // import css
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Player from './components/player'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Player />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
