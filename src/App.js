60a632c6ea7a1e045aacefdcd477c8e3451baec8
Jeremy Block <j.block@ufl.edu>
2020-08-13 11:06:33 -0400

    fixes react-player

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Player from './components/player'
+import ReactPlayer from 'react-player'
 
 
 
@@ -9,7 +9,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="Player-container">
-          <Player />
+          <ReactPlayer 
+          width='100%'
+          height='100%'
+          controls
+          url='http://media.w3.org/2010/05/sintel/trailer.mp4' />
         </div>
       </header>
     </div>
