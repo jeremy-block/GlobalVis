32dd55cdaf06f7f1d3b032a412b856ff74a0d73f
Jeremy Block <j.block@ufl.edu>
2020-08-13 10:58:55 -0400

    Revert "Trying react-player instead of video-react"
    This reverts commit 1f112cadd9863808d8382aec15518361ee34392a.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "../node_modules/video-react/dist/video-react.css"; // import css
 
 import React from 'react';
 import './App.css';
-import ReactPlayer from 'react-player'
+import Player from './components/player'
 
 
 
@@ -11,11 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="Player-container">
-          <ReactPlayer 
-          width='100%'
-          height='100%'
-          controls
-          url='http://media.w3.org/2010/05/sintel/trailer.mp4' />
+          <Player />
         </div>
       </header>
     </div>
